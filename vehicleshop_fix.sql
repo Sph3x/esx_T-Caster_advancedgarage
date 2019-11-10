b9477e43bf32f737ee4c01471aa16850c4574ce2
BuildTools <unconfigured@null.spigotmc.org>
2019-11-10 23:24:28 +0200

    first upload
    this is the first version, no bug fixes you should fix them if you want to use it in your server

---
+++ vehicleshop_fix.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.6-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table essentialmode.owned_vehicles
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `owner` varchar(30) COLLATE utf8mb4_bin NOT NULL,
+  `plate` varchar(12) COLLATE utf8mb4_bin NOT NULL,
+  `vehicle` longtext COLLATE utf8mb4_bin NOT NULL,
+  `type` varchar(20) COLLATE utf8mb4_bin NOT NULL DEFAULT 'car',
+  `job` varchar(20) COLLATE utf8mb4_bin NOT NULL DEFAULT '',
+  `stored` tinyint(1) NOT NULL DEFAULT 0,
+  `location` longtext COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
